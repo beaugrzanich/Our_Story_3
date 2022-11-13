411731235d33b2e526c6d92e81dd1b499423cafd
Beau Grzanich <beau@tanda.co>
2022-11-13 18:55:56 +0000

    change logos

--- config/initializers/logo.rb
+++ config/initializers/logo.rb
@@ -3,4 +3,4 @@
 # You may want to play with this setting after replacing the default Bullet Train starter kit logo with something else.
 # For example, if your logo is more squarish in aspect ratio, you probably want to increase this. However, remember you
 # must `rails restart` the server in order to see this setting take effect locally.
-BulletTrain::Themes.logo_height = 54
+BulletTrain::Themes.logo_height = 108
